6c529191a8cca0d7d4153ed2a5af9276a3bcde49
tranhuudat1509 <147556884+tranhuudat1509@users.noreply.github.com>
2023-12-04 21:02:05 -0800

    Completed and working (tested) version

--- TP.py
+++ TP.py
@@ -8,6 +8,8 @@ board = [
 ]
 
 # Create a function to display the Tic-Tac-Toe board
+
+
 def display_board(board):
     for row in board:
         print(row)
@@ -16,63 +18,83 @@ def display_board(board):
 
 
 def check_winner(board, player):
-    # Check rows
+    # Check rows, columns, and diagonals
     for row in board:
         if all(cell == player for cell in row):
             return True
 
-    # Check columns
     for col in range(3):
         if all(board[row][col] == player for row in range(3)):
             return True
 
-    # Check diagonals
-    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+    # Check main diagonal
+    if all(board[i][i] == player for i in range(3)):
+        return True
+
+    # Check secondary diagonal
+    if all(board[i][2-i] == player for i in range(3)):
+        return True
+
+    # Check for a tie
+    if all(cell != " " for row in board for cell in row):
         return True
 
     return False
 
 # Create a function to get the user's move
+
+
 def get_user_move():
-    isXValid = False
-    isYValid = False
-    while isXValid == False:
+    while True:
         try:
             x = int(input('Please choose a row from 1 to 3: ')) - 1
             if x not in range(3):
-                print('Invalid row number, please try again:')
-            else: isXValid = True
-        except ValueError:
-            print('Please enter a number:')             
-    while isYValid == False:
-        try:
+                print('Invalid row number, please try again.')
+                continue
+
             y = int(input('Please choose a column from 1 to 3: ')) - 1
-            if x not in range(3):
-                print('Invalid column number, please try again:')
-            else: isYValid = True  
+            if y not in range(3):
+                print('Invalid column number, please try again.')
+                continue
+
+            if board[x][y] != " ":
+                print('Position already taken, please choose another position.')
+
+            return x, y
         except ValueError:
-            print('Please enter a number')   
-    return x, y
+            print('Please enter a number.')
+
 
 # Create a function to get the computer's move
 
 
-# def get_computer_move():
+def get_computer_move():
+    empty_cells = [(i, j) for i in range(3)
+                   for j in range(3) if board[i][j] == " "]
+    return random.choice(empty_cells)
+
 
 # Create a function to save the game moves to a file
 SAVE_FILE = 'moves.txt'
 
+
 def save_moves_to_file(moves):
     with open(SAVE_FILE, 'a+') as save_file:
-        save_file.writelines(moves)
+        for move in moves:
+            save_file.writelines(f"{move[0]}, {move[1]}\n")
 
 
 # Main game loop
 
 # Create a function to initialize the game
 def initialize_game():
+    while True:
+        user_symbol = input("Choose your symbol (X or O): ").upper()
+        if user_symbol in ['X', 'O']:
+            break
+        else:
+            print("Invalid symbol. Please choose X or O.")
     player = "User"
-    user_symbol = input("Choose your symbol (X or O): ").upper()
     moves = []
     game_over = False
     return player, user_symbol, moves, game_over
@@ -80,28 +102,30 @@ def initialize_game():
 
 def main():
     player, user_symbol, moves, game_over = initialize_game()
+    display_board(board)
 
     while not game_over:
-        display_board(board)
+
         if player == "User":
             user_move = get_user_move()
             moves.append(user_move)
             board[user_move[0]][user_move[1]] = user_symbol
             game_over = check_winner(board, user_symbol)
+            if game_over:
+                break
         else:
+            opponent_symbol = "O" if user_symbol == "X" else "X"
             computer_move = get_computer_move()
             moves.append(computer_move)
-            board[computer_move[0]][computer_move[1]
-                                    ] = "O" if user_symbol == "X" else "X"
-            game_over = check_winner(board, "O" if user_symbol == "X" else "X")
+            board[computer_move[0]][computer_move[1]] = opponent_symbol
+            game_over = check_winner(board, opponent_symbol)
+            display_board(board)
+            if game_over:
+                break
 
         player = "User" if player == "Computer" else "Computer"
-
-    display_board(board)
     if game_over:
         print("The winner is: " + player)
-
-    # save_moves_to_file(moves)
 
 
 # Call the main function to start the game
